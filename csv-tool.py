c885b4eecb89721b7d271cd2466e24eb90a7c8ba
Rhett Trickett <RhettTrickett@users.noreply.github.com>
2016-06-22 10:57:23 +0200

    Added CSVFile class with rowCount method

---
+++ csv-tool.py
@@ -0,0 +1,21 @@
+import csv
+
+class CSVFile(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def rowCount(self):
+        with open(self.file_name, 'rU') as f:
+            reader = csv.reader(f, dialect=csv.excel)
+            data = list(reader)
+            row_count = len(data)
+            return row_count
+
+file_name = raw_input("Please enter filename: ")
+
+user_file = CSVFile(file_name)
+
+print "Row count is: " + str(user_file.rowCount())
+
+
+
